a7de54387008a728c390b93c10f35d083feff2f5
Philip Luk <fortyfunbobby@gmail.com>
2018-10-01 11:43:15 -0400

    postgresql to start up script

--- hackthebox/scripts/sys/init.kali_vm.sh
+++ hackthebox/scripts/sys/init.kali_vm.sh
@@ -1,3 +1,10 @@
 #! /bin/bash
 
 sudo /etc/init.d/ssh start
+sudo /etc/init.d/postgresql start
+# sudo systemctl enable vsftpd
+# sudo systemctl restart vsftpd
+
+# https://127.0.0.1:9392
+# admin:cf4b6658-24c3-4787-a322-1c122c9ee579
+# sudo openvas-start 
